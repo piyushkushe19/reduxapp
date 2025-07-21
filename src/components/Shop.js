a0f53eb4aba1bf8c180dd0d4dd252615d4eedf8b
Piyush Kushe <159922215+piyushkushe19@users.noreply.github.com>
2025-07-21 21:09:07 +0530

    Installed redux react-redux redux-thunk

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-    <h2>Buy Adidas Shoes 50rs</h2>
+    <h2>Deposit/Withdraw Money</h2>
       <button className="btn-primary mx-2">-</button>
-      Add this Item to cart
+      Update Balance
       <button className="btn-primary mx-2">+</button>
     </div>
   )
